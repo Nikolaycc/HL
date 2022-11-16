eee3b3ed5fc80a47f83e35fdb41c4ba97813d441
Nikolay <nikolay@pop-os.localdomain>
2022-11-16 14:43:47 +0400

    Added tests for HL_Get(..)

--- test/test.c
+++ test/test.c
@@ -1,6 +1,7 @@
 #include "../src/hl.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void hello() {
     log("Hello 1");
@@ -28,16 +29,36 @@ int main(void) {
     int ind2 = HL_Register_Callback(&app, (callback_res_t)hello2);
     int ind3 = HL_Register_Callback(&app, (callback_res_t)hello3);
 
-    //HL_Get(&app, "/user", exmp);
+//    HL_Get(&app, "/user", exmp);
 
-    Res res1 = HL_Get_Callback(app.cl, ind)();
-    Res res2 = HL_Get_Callback(app.cl, ind2)();
-    Res res3 = HL_Get_Callback(app.cl, ind3)();
+    Res res1 = HL_Get_Callback(app, ind)();
+    Res res2 = HL_Get_Callback(app, ind2)();
+    Res res3 = HL_Get_Callback(app, ind3)();
+
+    char* string = HL_Format("AYEO %s hello\n", res1.res);
+
+    log(string);
 
     log(res1.res);
 
     log(res2.res); // NULL
     log(res3.res); // NULL
+
+    HL_Get(&app, "/", Index);
+
+    char tst[20];
+
+    sm_get(app.routes, "/", tst, 2);
+
+    log(HL_Format("sm_get return -> %s", tst));
+
+    int yum = atoi(tst);
+
+    log(HL_Format("yum = %d", yum));
+
+    Res test = HL_Get_Callback(app, yum)();
+
+    log(HL_Format("RES: %s STATUS: %d", test.res, test.status));
 
     HL_Listen(&app);
 
